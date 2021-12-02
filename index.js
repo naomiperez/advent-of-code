2d8156504f88f988e5fcc4270b05ca8f60d4b47d
Naomi Perez <naomiperez@naomis-air.lan>
2021-12-01 18:19:37 -0800

    Completed Day 1: Sonar Sweep

--- index.js
+++ index.js
@@ -1,34 +1,4 @@
-/* ------------------------------ Day 1 ------------------------------ */
-const fs = require("fs");
+const { dayOnePart1, dayOnePart2 } = require('./days/day1');
 
-function getInput() {}
-
-function countIncreasedMeasurements(measurements) {
-  let count = 0;
-
-  for (let i = 1; i < measurements.length; i++) {
-    if (measurements[i] > measurements[i - 1]) {
-      count++;
-    }
-  }
-
-  return count;
-}
-
-function getInput(fileName) {
-  try {
-    const data = fs.readFileSync(fileName, "utf8");
-    return data;
-  } catch (err) {
-    console.error(err);
-  }
-}
-
-function dayOne() {
-  const inputs = getInput("inputs/day1-inputs.txt");
-  let inputsArr = inputs.split("\n").map(element => parseInt(element));
-
-  return countIncreasedMeasurements(inputsArr);
-}
-
-console.log(dayOne());
+//console.log(dayOnePart1());
+console.log(dayOnePart2());
